7d693b9adc39dea17d090401166fa50d5d1c4dfc
Sapatoxx <sebastianmendozab18@gmail.com>
2025-10-25 23:51:10 +0200

    Creación de evento de cambio de Staking Period

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -6,7 +6,43 @@ contract stakingApp is Ownable {
 
   //variables
   address public stakingToken;
-  constructor(address _stakingToken, address owner_) Ownable(owner_) {
+
+  //Events
+  event changePeriod(uint256 newStakingPeriod_);
+  constructor(address _stakingToken, address owner_, uint256 stakingPeriod_) Ownable(owner_) {
     stakingToken = _stakingToken;
+    stakingPeriod = stakingPeriod_;
   }
+
+  ///////FUNCTIONS
+
+  ////External functions
+  //Deposit
+
+  //Withdraw
+
+  //Claim rewards
+
+  ////Internal functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function changeStakingPeriod(uint256 newStakingPeriod_)  external onlyOwner {  //Colocamos el modificador onlyOwner para que solo el dueño del contrato pueda cambiar el periodo de staking, este modificador viene de la libreria OpenZeppelin
+    stakingPeriod = newStakingPeriod_; 
+    emit changePeriod(newStakingPeriod_);
+  }
+
+
+
+
 }
